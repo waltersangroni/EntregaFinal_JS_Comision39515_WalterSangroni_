8ff7df2a4768a815c5048469269edb428ad67a4d
Walter Sangroni <walterhugosangroni@gmail.com>
2023-05-13 16:14:51 -0300

    reemplazando la segunda pre por la tercera pre entrega

--- main.js
+++ main.js
@@ -1,202 +1,178 @@
 // Variables globales
 
 const carrito = [];
-let precioCarrito = 0;
+const contenedor_catalogo = document.querySelector(".main_container")
+const contenedor_carrito = document.querySelector(".carrito_container")
 
 // Objetos
 
-function Producto(nombre, color, precio, categoria) {
+function Producto(id, img, nombre, precio) {
+  this.id = id;
+  this.img = img;  
   this.nombre = nombre;
-  this.color = color;
   this.precio = precio;
-  this.categoria = categoria;
 }
 
-const producto1 = new Producto("Remera", "Negra", 3500);
-const producto2 = new Producto("Pantalon", "Azul", 6500);
-const producto3 = new Producto("Buzo", "Rojo", 5500);
-const producto4 = new Producto("Campera", "Negra", 9500);
+const producto1 = new Producto("1", "img/moda1.JPG", "Remera negra", 3500);
+const producto2 = new Producto("2","img/moda2.JPG", "Remera y pantalon", 6500);
+const producto3 = new Producto("3","img/moda3.JPG","Remara blanca", 3500);
+const producto4 = new Producto("4","img/moda4.JPG","Piloto lluvia", 9500);
+const producto5 = new Producto("5","img/moda5.JPG","Saco otoño", 8500);
+const producto6 = new Producto("6","img/moda6.JPG","Remera verde", 3000);
+const producto7 = new Producto("7","img/moda7.JPG","Camisa", 4500);
+const producto8 = new Producto("8","img/moda8.JPG","Musculosa", 3000);
+const producto9 = new Producto("9","img/moda9.JPG","Pollera", 4500);
+const producto10 = new Producto("10","img/moda10.JPG","Remera clasica", 2500);
+const producto11 = new Producto("11","img/moda11.JPG","Pantalon", 5500);
+const producto12 = new Producto("12","img/moda12.JPG","Camisa verde", 4500);
 
 // Arrays
 
 const listaProductos = [
-    producto1, producto2, producto3, producto4
+    producto1, producto2, producto3, producto4, producto5, producto6, producto7, producto8, producto9, producto10, producto11, producto12
 ];
 
-// Funciones
+let total_compra = carrito.reduce((acum, producto)=>{
+    return acum + producto.precio
+}, 0)
 
-const agregarAlCarrito = (producto) => {
-    carrito.push(producto);
-    
-    precioCarrito += producto.precio;
+console.log(total_compra);
 
-    mostrarActualizacion();
+
+// ---------------------MAIN---------------------
+
+// CARGAR CATALOGO
+
+cargarProductos();
+
+// EVENTO AGREGAR PRODUCTOS AL CARRITO
+
+// agarro todos los botones del catalogo
+const botonesAgregarAlCarrito = document.querySelectorAll('.producto_agregar');
+
+// para cada boton agrego un eventListener
+botonesAgregarAlCarrito.forEach(boton => {
+  boton.addEventListener('click', () => {
+    const producto = obtenerProducto(boton);
+    agregarAlCarrito(producto);
+  });
+});
+
+// EVENTO ELIMINAR PRODUCTOS AL CARRITO
+
+const botonesEliminarDelCarrito = document.querySelectorAll(".producto_eliminar")
+
+botonesEliminarDelCarrito.forEach(boton => {
+    boton.addEventListener('click', () => {
+      const producto = obtenerProducto(boton);
+      eliminarProducto(producto);
+    });
+  });
+
+  function eliminarProducto (){
+    carrito.remove()
+  }
+
+
+
+// EVENTO FORMULARIO
+
+const formularioContainer = document.querySelector(".formulario_container")
+const input_Name = document.querySelector("#inputName")
+const input_Email = document.querySelector("#inputEmail")
+
+formularioContainer.addEventListener("submit", validarFormulario)
+
+function validarFormulario(e){
+    e.preventDefault()
+    console.log(`Nombre: ${input_Name.value}`)
+    console.log(`Email: ${input_Email.value}`)
 }
 
-/*const quitarDelCarrito = (producto) => {
-    const indice = carrito.findIndex(producto);
-    carrito.splice(indice, 1);
+// ---------------------FUNCIONES---------------------
 
-    precioCarrito -= producto.precio;
-
-    mostrarActualizacion();
-} No supe incorporar la funcion de eliminar del carrito*/
-
-const mostrarActualizacion = () =>  {
-    console.log("Producto agregado al carrito\nCantidad de productos: " + carrito.length + "\nTotal: " + precioCarrito);
-}
-
-const mostrarCarrito = () => {
-    console.log("\n\n\nCARRITO\n\n")
-    for(i=0; i < carrito.length; i++) {
-        console.log("Nombre: " + carrito[i].nombre + "\nPrecio: " + carrito[i].precio + "\n\n");
+function cargarProductos() {
+    for (let producto of listaProductos){
+        const tarjeta = crearTarjeta(producto, "CATALOGO");
+        mostrarTarjeta(tarjeta, contenedor_catalogo);
     }
-    console.log("\nTOTAL: " + precioCarrito);
 }
 
-// Main
+function mostrarTarjeta(tarjeta, contenedor) {
+    contenedor.appendChild(tarjeta);
+}
 
-let respuestaUsuario = Number(prompt("¿Que producto desea agregar al carrito? \n1.Remera \n2.Pantalon \n3.Buzo\n4.Campera\n0.Salir"));
-let nombreProducto = "";
+function crearTarjeta(producto, tipoTarjeta) {
+    let tarjeta = document.createElement("section");
 
-while(respuestaUsuario != "0") {
-    switch (respuestaUsuario) {
-        case 1:
-            nombreProducto = "Remera"
+    switch(tipoTarjeta){
+        case "CATALOGO":
+            tarjeta.innerHTML = 
+                `
+                <img class="producto_imagen" src=${producto.img} alt="modelos1">
+                <div class="producto_detalles">
+                    <h2 class="producto_titulo">${producto.nombre}</h2>
+                    <p class="producto_precio">$ ${producto.precio}</p>
+                    <button class="producto_agregar" data-producto-id=${producto.id}>Agregar al carrito</button>
+                </div>
+                `;
             break;
-        case 2:
-            nombreProducto = "Pantalon"
-            break;
-        case 3:
-            nombreProducto = "Buzo"
-            break;
-        case 4:
-            nombreProducto = "Campera"
-            break;
-        case 0:
-            nombreProducto = "Salir";
-            break;
-        case NaN:
-            nombreProducto = "NULL";
-        default:
-            nombreProducto = "NULL";
+        case "CARRITO":
+            tarjeta.innerHTML = 
+                `
+                
+                <div class="producto_detalles">
+                    <h2 class="producto_titulo">${producto.nombre}</h2>
+                    <p class="producto_precio">$ ${producto.precio}</p>
+                    <button class="producto_eliminar" data-producto-id=${producto.id}>Eliminar del carrito</button>
+                </div>
+                `;
             break;
     }
-    
-    if(nombreProducto == "NULL") {
-        alert("Por favor ingrese un numero");
-    } else if (nombreProducto == "Salir") {
-        console.log("Vuelva Pronto")
+
+    return tarjeta;
+}
+
+function obtenerProducto(boton) {
+    // obtengo el id del producto segun el boton que clickee
+    const producto_id = boton.dataset.productoId; //ver abajo
+
+    // con esa id buscamos el producto en la lista
+    return producto = listaProductos.find((producto) => {return producto.id == producto_id;});
+}
+
+function agregarAlCarrito(producto) {
+    // si el producto ya estaba en el carrito no lo muestro
+    let yaExiste = carrito.includes(producto);
+    if(yaExiste) {
+        aumentarCantidad(producto);
     }
     else {
-        let productoSeleccionado = listaProductos.find((producto) => producto.nombre === nombreProducto);
-        agregarAlCarrito(productoSeleccionado);
+        const tarjeta = crearTarjeta(producto, "CARRITO");
+        mostrarTarjeta(tarjeta, contenedor_carrito);
     }
 
-    respuestaUsuario = Number(prompt("¿Que producto desea agregar al carrito? \n1.Remera \n2.Pantalon \n3.Buzo\n4.Campera\n0.Salir"));
-}
-
-mostrarCarrito();
-
-
-// Practicando JS
-
-/*let miOpcion = generarOpcion();
-let respuesta = prompt("piedra")
-
-while(seguimosJugando) {
-    switch(respuesta){
-        case "piedra": 
-                if(miOpcion == "papel"){
-                    ganeYo();
-                } else if(miOpcion == "tijera") {
-                    perdiYo();
-                } else empatamos();
-            break;
-        case "tijera":
-            break;
-        case "papel":
-            break;
-        default:
-                
-            break;
-    }
+    carrito.push(producto);
 }
 
 
-function ganeYo() {
-    console.log();
-    if(rondasGanadasPorMi == 3) {
-        console.log();
-        seguimosJugando = false;
-    }
-}*/
-
-// let gradosCelsius = prompt("Escribe los grados Celsius");
-
-// let gradosFarenheit = celsiusAFarenheit()
-
-// function celsiusAFarenheit(gradosCelsius) {
-//     let gradosFarenheit = gradosCelsius * 1.8 + 32;
-//     return gradosFarenheit;
-// }
-
-// console.log("Los grados Farenheit son: " + gradosFarenheit);
-
-// let precio = Number(prompt("¿Cuanto costo la cena?"));
-// let propina = calcularPropina(precio);
-
-// function calcularPropina(precio) {
-//     let propina = precio * 0.1;
-//     return propina;
-// };
-
-// console.log("La propina que tenes que dejar es de: " + propina);
 
 
+function aumentarCantidad(producto) {
+    producto.cantidad++;
+    //actualizar html con la cantidad nueva
+}
 
-// Lista de tareas pendientes que permita agregar, eliminar y marcar tareas como completadas.
+//esta funcion me la paso chatGPT
+//function limpiarCarrito() {
+//    while (carritoContainer.firstChild) {
+//        carritoContainer.removeChild(carritoContainer.firstChild);
+//      }
+//}
 
-// function Tarea(trabajo, horas, boolCompletado) {
-//     this.trabajo = trabajo;
-//     this.horas = horas;
-//     this.boolCompletado = boolCompletado;
-//   }
+// Dom eliminar elemento se hace con .remove()
 
-//   const tareaAlbanil = new Tarea("Albañil", 8, true);
-//   const tareaFotografo = new Tarea("Fotografo", 8, false);
-//   const tareaProgramador = new Tarea("Programador", 8, true);
-
-
-// let tareasPendientes = [tareaAlbanil, tareaFotografo];
-// let respuestaUsuario1 = prompt("¿Queres agregar la tarea de programador? \n1-Si \n2-No");
-
-// if(resrespuestaUsuario1 == "1") {
-//     agregarTarea(tareaProgramdor);
-// } else {
-//     console.log("ok");
-// }
-
-// function agregarTarea(tarea) {
-//     tareasPendientes.push(tarea);
-// }
-
-// function eliminarTarea(tarea) {
-//     tareasPendientes.pop(tarea);
-//     tareasCompletadas.push(tarea);
-// }
-
-// function mostrarTareas() {
-//     console.log("\n\n\TAREAS\n\n")
-//     for(i=0; i < tareasPendientes.length; i++) {
-//         console.log("Trabajo: " + carritareasPendientes[i].trabajo + "\nHoras: " + tareasPendientes[i].horas + "\n\n");
-//     }
-// }
-
-// mostrarTareas();
-
-
-
-
-
-
+// EXPLICACION DATASET
+// <button class="producto_agregar" data-producto_id=${producto.id}>Agregar al carrito</button>
+// tenemos un boton que tiene una clase "producto_agregar" y un data "producto_id" que lo llamas a traves de 
+// la funcion dataset, que es una funcion que ya existe de javascript
